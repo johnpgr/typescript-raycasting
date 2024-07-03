f8b61b23632e8b2f508ef9b98e7b841cdedbc738
johnpgr <jp.greidinger@hotmail.com>
2024-07-03 11:06:01 -0300

    rm workflow

--- consts.js
+++
@@ -1,9 +0,0 @@
-export const EPS = 1e-6;
-export const FACTOR = 80;
-export const SCREEN_WIDTH = 300;
-export const FAR_CLIPPING_PLANE = 10.0;
-export const DEFAULT_MOVESPEED = 2.0;
-export const DEFAULT_TURN_SPEED = Math.PI * 0.75;
-export const FOV = Math.PI * 0.5;
-export const NEAR_CLIPPING_PLANE = 0.25;
-//# sourceMappingURL=consts.js.map
